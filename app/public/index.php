22d4b02b6b8511cb00cf22b4e289a03f4a4ab270
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-04-13 14:14:41 -0300

    feat: start application

---
+++ app/public/index.php
@@ -0,0 +1,16 @@
+<?php require "../functions/pages.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>Curso PHP devclass</title>
+</head>
+<body>
+    <div class="container">
+        <?php require load(); ?>
+    </div>
+</body>
+</html>
