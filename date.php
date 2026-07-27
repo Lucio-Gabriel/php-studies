67b402fd33ec322edc63d2b37f5ea4750971f79f
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-07-27 07:36:34 -0300

    date in php

---
+++ date.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set('America/Sao_Paulo');
+
+echo "Hoje é dia: " . date('d/m/y');
+echo "<br>";
+echo " agora são " . date('g.i: a');
+echo "<br>";
+echo "Estamos no mês: " . date('F');
+echo "<br>";
+echo "Dia da semana: " . date("l");
